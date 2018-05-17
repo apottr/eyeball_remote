49c4867b0f9e0bd1fac7dee8f83a8969b1492dd1
Amelia <amy@Amelias-MacBook-Pro.local>
2018-05-17 18:25:00 -0400

    switched from rpc to REST, wrote basic routes

--- server.go
+++ server.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func routeAddJob(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "Add Job"}`)
+}
+
+func routeDelJob(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "Del Job"}`)
+}
+
+func routeCheckCron(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "Check Cron"}`)
+}
+
+func routePauseJob(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"status": "Pause Job"}`)
+}
 
 func main() {
-	fmt.Println("hello, world!")
+	http.HandleFunc("/add_job", routeAddJob)
+	http.HandleFunc("/del_job", routeDelJob)
+	http.HandleFunc("/pause_job", routePauseJob)
+	http.HandleFunc("/check_cron", routeCheckCron)
+	log.Fatal(http.ListenAndServeTLS(":8081", "server.crt", "server.key", nil))
 }
