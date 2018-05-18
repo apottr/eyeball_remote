150c3dc4c0911f42d3e2c2d884a3f55af5b326ac
Amelia <apottr@akira.local>
2018-05-18 00:52:48 -0400

    help

--- server.go
+++ server.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/robfig/cron"
 )
+
+var c *cron.Cron
 
 func routeAddJob(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	c.AddFunc("")
 	fmt.Fprintf(w, `{"status": "Add Job"}`)
 }
 
@@ -27,6 +32,7 @@ func routePauseJob(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	c = cron.New()
 	http.HandleFunc("/add_job", routeAddJob)
 	http.HandleFunc("/del_job", routeDelJob)
 	http.HandleFunc("/pause_job", routePauseJob)
