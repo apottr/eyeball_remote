37958397abb758e752e517af37de7ea04fe2fe59
Amelia <amy@tachikoma.local>
2018-07-17 16:45:11 -0400

    added log items, changed time from 41 days to 1 minute

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from es_ops import get_sources_for_region
 from crontab import CronTab
 from pathlib import Path
-import os,time
+from time import sleep,gmtime,asctime
+import os
 
 directory = Path(__file__).parent.parent.resolve() #pylint: disable=no-member
 if "VIRTUAL_ENV" in os.environ:
@@ -22,10 +23,12 @@ def checker(region):
     src = get_sources_for_region(region)
     for item in src:
         if len(list(cron.find_comment(item["id"]))) == 0:
+            print("adding item",item["id"])
             add_job_to_cron(item)
     src_id = [item["id"] for item in src]
     for item in cron:
         if item.comment not in src_id:
+            print("deleting item",item.comment)
             cron.remove(item)
     cron.write()
 
@@ -33,5 +36,6 @@ def checker(region):
 if __name__ == "__main__":
     hn = os.environ["HOSTNAME"]
     while True:
+        print("running at ",asctime(gmtime))
         checker(hn)
-        time.sleep(60000)
+        sleep(60)
